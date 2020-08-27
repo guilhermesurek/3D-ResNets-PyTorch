48a6c0f3e414b69425af278a88f041a96681cb0f
Guilherme Surek <guilhermesurek@gmail.com>
2020-08-27 15:04:34 -0300

    Generate checking jpg files report

---
+++ util_scripts/activitynet/file_report.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import timeit
+
+start = timeit.default_timer()
+#dir_path = 'E:/Mestrado/dataset/act200/train_val'
+dir_path = 'D:/Mestrado/Dataset/dst/activitynet200/train_val'
+
+if __name__ == "__main__":
+    c=0
+    # List video's folders
+    file_list = list(sorted(os.listdir(dir_path)))
+    # Create empty df
+    df = pd.DataFrame(columns=['video_id', 'qty_files'])
+    for eFile in file_list:
+        # Make subfolder path
+        mk_path = os.path.join(dir_path, eFile)
+        # Count files in it
+        files_number = len(os.listdir(mk_path))
+        # Save in dict
+        aux = {
+            'video_id': eFile,
+            'qty_files': files_number
+        }
+        # append to df
+        df = df.append(aux, ignore_index=True)
+        #print(f"Folder {eFile} has {files_number} files in it.")
+        c+=1
+        print(f"c:{c}")
+    # Make excel dst path
+    dst = os.path.dirname(os.path.realpath(__file__))
+    dst_path = os.path.join(dst, 'output.xlsx')
+    df.to_excel(dst_path)
+    # Print head
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
